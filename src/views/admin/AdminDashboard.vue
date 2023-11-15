06beef7ab09b329576c4c8cb0cec493a5ab82787
SoulYaKnow <118219430+soulyaknow@users.noreply.github.com>
2023-11-15 15:35:39 +0800

    admin dashboard update

--- src/views/admin/AdminDashboard.vue
+++ src/views/admin/AdminDashboard.vue
@@ -102,10 +102,10 @@
                                 </tr>
                                 <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        Icey
+                                        Lenner
                                     </th>
                                     <td class="px-6 py-4">
-                                        ordbej@gmail.com
+                                        lennersoliano@gmail.com
                                     </td>
                                     <td class="px-6 py-4">
                                         BSHM
@@ -121,13 +121,13 @@
                                 </tr>   
                                 <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        Icey
+                                        Erik
                                     </th>
                                     <td class="px-6 py-4">
-                                        ordbej@gmail.com
+                                        erikredolosa@gmail.com
                                     </td>
                                     <td class="px-6 py-4">
-                                        BSHM
+                                        BEED
                                     </td>
                                     <td class="px-6 py-4">
                                         Active
