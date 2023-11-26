69ef506b9e6eebb4339b5bc7ea7618e0812b43b3
SoulYaKnow <118219430+soulyaknow@users.noreply.github.com>
2023-11-27 03:29:51 +0800

    create modal design

--- src/components/CreateModal.vue
+++ src/components/CreateModal.vue
@@ -1,95 +1,112 @@
 <template>
       <!-- Modal container -->
-    <div v-if="showCreateModal" class="fixed inset-0 bg-sky-950 bg-opacity-5 items-center justify-center font-Times New Roman">
-        <div class="bg-white p-8 max-w-md mx-auto mt-48 border-4 border-sky-950 rounded-xl max-h-[26rem] overflow-y-auto">
+    <div v-if="showCreateModal" class="fixed inset-0 bg-sky-950 bg-opacity-5 items-center justify-center font-poppins">
+        <div class="bg-white p-8 md:max-w-lg max-w-sm mx-auto mt-48 border-4 border-sky-950 rounded-xl max-h-[26rem] overflow-y-auto">
             <button @click="handleClose" id="close-btn">
                 <i class="bi bi-x-lg"></i>
                 <span></span>
             </button>
+            <!-- Modal header -->
             <div class="mb-4 flex flex-col">
-                <h2 class="text-2xl font-semibold text-center bg-sky-950 text-white mb-2">Create Teacher</h2>
+              <h2 class="text-2xl font-semibold text-center bg-sky-950 text-white mb-2" v-if="personelType == 1">Create Instructor</h2>
+              <h2 class="text-2xl font-semibold text-center bg-sky-950 text-white mb-2" v-if="personelType == 2">Create Guard</h2>
+              <h2 class="text-2xl font-semibold text-center bg-sky-950 text-white mb-2" v-if="personelType == 3">Create Canteen-staff</h2>
             </div>
             <!-- Modal body -->
             <div class="mb-4">
-                <div>
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="fullname">Full Name</label>
-                    <input v-model="name" class="border rounded w-full py-2 px-3" id="fullname" type="text" placeholder="Enter Full name">
-                    <p v-if="errors.name">{{ errors.name }}</p>
-                </div>
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="shift">Shift</label>
-                    <select v-model="shift" class="border rounded text-gray-700 w-full py-2 px-3" id="shift">
-                        <option value=0>Part Time</option>
-                        <option value=1>Full Time</option>
-                    </select>
-                </div>
-                <div class="mb-4 flex">
-                   <div class="mr-10">
-                      <label class="block text-gray-700 text-sm font-bold mb-2" for="personal-type">Personal Type</label>
-                      <select v-model="personelType" class="border rounded text-gray-700 w-full py-2 px-3" id="personal-type">
-                         <option v-for="entity in entities" :value="entity.id">{{ entity.entity_name }}</option>
+                <!-- Modal content header -->
+                <div class="mb-4 flex flex-col">
+                  <div class="grid gap-x-2 lg:grid-cols-2 sm-grid-cols-2 grid-cols-2">
+                    <div>
+                      <label class="flex text-gray-700 text-sm font-bold mb-2" for="fullname">Full Name</label>
+                      <input v-model="name" class="border rounded w-full py-2 px-3 md:h-10" id="fullname" type="text" placeholder="Enter Full name">
+                      <p v-if="errors.name">{{ errors.name }}</p>
+                    </div>
+                    <div>
+                      <label class="flex text-gray-700 text-sm font-bold mb-2" for="shift">Shift</label>
+                      <select v-model="shift" class="cursor-pointer border rounded text-gray-700 w-full py-2 px-3 h-10" id="shift">
+                          <option value=0>Part Time</option>
+                          <option value=1>Full Time</option>
                       </select>
-                   </div>
-                   <div v-if="personelType == 1">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="department">Department</label>
-                    <select v-model="department" class="border rounded text-gray-700 w-full py-2 px-3" id="department">
-                        <option v-for="department in departments" :value="department.id">{{ department.department }}</option>
-                    </select>
-                   </div>
+                    </div>
+                    <div class="pt-2">
+                      <label class="flex text-gray-700 text-sm font-bold pb-2" for="personal-type">Personal Type</label>
+                      <select v-model="personelType" class="cursor-pointer border rounded text-gray-700 w-full py-2 px-3 h-10" id="personal-type">
+                        <option v-for="entity in entities" :value="entity.id">{{ capitalizeFirstLetter(entity.entity_name) }}</option>
+                      </select>
+                    </div>
+                  </div>            
+                </div>  
+                <!-- End modal content body -->           
+                <!-- Modal content body -->
+                <div class="mb-4 flex flex-col">
+                  <div class="grid md:gap-x-2 md:lg:grid-cols-3 md:sm-grid-cols-3 gap-x-2 gap-y-2 grid-cols-3 sm-grid-cols-3">                  
+                    <div v-if="personelType == 1">
+                      <label class="flex text-gray-700 text-sm font-bold" for="subject">Subject</label>
+                        <select v-model="subject" class="cursor-pointer border rounded text-gray-700 w-full py-2 px-3 pr-2" id="subject">
+                          <option v-for="subject in subjects" :value="subject.id">{{ capitalizeFirstLetter(subject.name) }}</option>
+                      </select>   
+                    </div>
+                    <div v-if="personelType == 1">
+                      <label class="flex text-gray-700 text-sm font-bold w-40" for="section-year">Section & Year</label>
+                      <select v-model="sectionYear" class="cursor-pointer border rounded text-gray-700 w-full py-2 px-3" id="section-year">
+                          <option v-for="sectionYear in sectionYears" :value="sectionYear.id">{{ sectionYear.year_section }}</option>
+                      </select>
+                    </div>
+                    <div v-if="personelType == 1">
+                      <label class="block text-gray-700 text-sm font-bold" for="department">Department</label>
+                      <select v-model="department" class="cursor-pointer border rounded text-gray-700 w-full py-2 px-3" id="department">
+                        <option v-for="department in departments" :value="department.id">{{ department.department.toUpperCase() }}</option>
+                      </select>
+                    </div>
+                    <div v-if="personelType == 1">
+                      <label class="flex text-gray-700 text-sm font-bold" for="time">Time</label>
+                      <input v-model="time" class="border rounded w-full h-9 px-3" id="time" type="text" placeholder="Eg. 1:00-2:30 PM">
+                      <p v-if="timeError">{{ timeError }}</p>
+                    </div>
+                    <div v-if="personelType == 1">
+                      <label class="flex text-gray-700 text-sm font-bold" for="day">Day</label>
+                      <input v-model="day" class="border rounded w-full h-9 px-3" id="day" type="text" placeholder="Eg. MWF, TTH, SAT">
+                      <p v-if="sYError">{{ sYError }}</p>
+                    </div>  
+                  </div>
+                  <div class="md:pl-[329px] pl-[200px] mt-2" v-if="personelType == 1">
+                    <button @click="addClassBtn" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl">Add Class</button>
+                  </div>
                 </div>
+                <!-- End modal content body -->
+                <!-- Modal content footer -->
+                <div class="mb-4">
+                  <p v-if="dataExist">{{ dataExist }}</p>
+                  <div v-if="showTable" class="shadow-md sm:rounded-lg mt-4 overflow-auto md:max-h-[111px] max-h-[200px] border-2 border-sky-950">
+                    <table class="max-w-screen w-full">
+                      <thead class="border-2 border-sky-950 text-center bg-sky-950 text-white">
+                        <th>Subject</th>
+                        <th>Sections</th>
+                        <th>Schedule</th>
+                        <th>Time</th>
+                        <th>Action</th>
+                      </thead>
+                      <tbody v-for="(klass,klassIndex) in classes" :key="klassIndex" class="md:text-center">
+                        <tr v-for="(sched,schedIndex) in klass.schedules"
+                          class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-200 even:dark:bg-gray-800 dark:border-gray-700
+                          border-b border-gray-200 hover:bg-gray-100">
+                          <td v-if="schedIndex == 0" :rowspan="klass.schedules.length">{{ capitalizeFirstLetter(klass.subject.name) }}</td>
+                          <td>{{ sched.sectionYear.year_section }}</td>
+                          <td>{{ sched.day.toUpperCase() }}</td>
+                          <td>{{ sched.time.toUpperCase() }}</td>
+                          <td>
+                            <button @click="editData(klass.subject.id,sched.sectionYear.id,sched.day,sched.time,klassIndex,schedIndex)">Edit</button>
+                            <button @click="removeRowData(klassIndex,schedIndex)">Remove</button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>                  
+                  <p v-if="errors.class && personelType == 1">{{ errors.class }}</p>
+                </div>
+                <!-- End modal content footer -->
             </div>
-            <div class="mb-4 flex" v-if="personelType == 1">
-              <div>
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="subject">Subject</label>
-                <select v-model="subject" class="border rounded text-gray-700 w-full py-2 px-3" id="subject">
-                    <option v-for="subject in subjects" :value="subject.id">{{ subject.name }}</option>
-                </select>
-              </div>
-              <div>
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="section-year">Section & Year</label>
-                <select v-model="sectionYear" class="border rounded text-gray-700 w-full py-2 px-3" id="section-year">
-                    <option v-for="sectionYear in sectionYears" :value="sectionYear.id">{{ sectionYear.year_section }}</option>
-                </select>
-              </div>
-            </div>
-            <div class="mb-4 flex" v-if="personelType == 1">
-              <div>
-                <label class=" text-gray-700 text-sm font-bold mb-2" for="time">Time</label>
-                <input v-model="time" class="border rounded w-full py-2 px-3" id="time" type="text" placeholder="Eg. 1:00-2:30 PM">
-                <p v-if="timeError">{{ timeError }}</p>
-              </div>
-              <div>
-                <label class=" text-gray-700 text-sm font-bold mb-2" for="day">Day</label>
-                <input v-model="day" class="border rounded w-full py-2 px-3" id="day" type="text" placeholder="Eg. MWF, TTH, SAT">
-                <p v-if="sYError">{{ sYError }}</p>
-              </div>
-              
-             <button @click="addClassBtn" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Add Class</button>
-            </div>
-            <p v-if="dataExist">{{ dataExist }}</p>
-            <table v-if="showTable">
-              <thead>
-                <th>Subject</th>
-                <th>Sections</th>
-                <th>Schedule</th>
-                <th>Time</th>
-                <th>Action</th>
-              </thead>
-              <tbody v-for="(klass,klassIndex) in classes" :key="klassIndex">
-                <tr v-for="(sched,schedIndex) in klass.schedules ">
-                  
-                  <td v-if="schedIndex == 0" :rowspan="klass.schedules.length">{{ klass.subject.name }}</td>
-                  <td>{{ sched.sectionYear.year_section }}</td>
-                  <td>{{ sched.day }}</td>
-                  <td>{{ sched.time }}</td>
-                  <td>
-                    <button @click="editData(klass.subject.id,sched.sectionYear.id,sched.day,sched.time,klassIndex,schedIndex)">Edit</button>
-                    <button @click="removeRowData(klassIndex,schedIndex)">Remove</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <p v-if="errors.class && personelType == 1">{{ errors.class }}</p>
             <!-- Modal footer -->
             <div class="flex justify-end">
                 <button @click="handleClickCreate" class="bg-sky-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -195,7 +212,9 @@ const addClassBtn = ()=>{
   }
 }
 
-
+const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
 
 const handleClickCreate = ()=>{
   if(name.value !== ''){
@@ -304,6 +323,21 @@ const handleClickCreate = ()=>{
   background-color: transparent;
 }
 .overflow-y-auto::-webkit-scrollbar-thumb {
+  background-color: rgb(8, 47, 73, 1);
+  border-radius: 8px;
+  width: 4px;
+}
+.overflow-auto::-webkit-scrollbar {
+  display: none;
+  width: 0px;
+}
+.overflow-auto:hover::-webkit-scrollbar {
+  display: block;
+}
+.overflow-auto::-webkit-scrollbar-track {
+  background-color: transparent;
+}
+.overflow-auto::-webkit-scrollbar-thumb {
   background-color: rgb(8, 47, 73, 1);
   border-radius: 8px;
   width: 4px;
